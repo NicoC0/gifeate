1c47c1087ff6576802f8c797ee5a09cf8bb9bae1
NicoC0 <nicoconfaa@gmail.com>
2022-07-30 14:52:23 -0300

    code refactoring

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import { useState, useEffect } from 'react'
-import './App.css';
-import getGifs from './services/getGifs';
+import React from "react";
+import "./App.css";
+import ListOfGifs from "./components/ListOfGifs";
 
 function App() {
-  
-  const [gifs, setGifs] = useState([])
-
-  useEffect(() => {
-    getGifs().then(gifs => setGifs(gifs))
-  }, [])
-
   return (
     <div className="App">
       <section className="App-content">
-        {
-          gifs.map(gif => {
-          return (
-            <div>
-              <h4>{gif.title}</h4>
-              <small>{gif.id}</small>
-              <img src={gif.url} alt={gif.title} />
-            </div>
-          )
-        })
-        }
+        <ListOfGifs keyword='Argertina' />
       </section>
-
     </div>
   );
 }

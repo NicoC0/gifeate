95e180f91b1c0f48851dccde3d86e971f70a8d6c
NicoC0 <nicoconfaa@gmail.com>
2022-07-30 14:51:51 -0300

    create component for display gifs

---
+++ src/components/ListOfGifs.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import Gif from "./Gif";
+import getGifs from "../services/getGifs";
+
+const ListOfGifs = ({ keyword }) => {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(() => {
+    getGifs({ keyword }).then((gifs) => setGifs(gifs));
+  }, [keyword]);
+
+  return gifs.map(({ id, title, url }) => (
+    <Gif key={id} id={id} title={title} url={url} />
+  ));
+};
+
+export default ListOfGifs;

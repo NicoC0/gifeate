893174bd6c6fe85cf33e93b8351c05ba8db45370
NicoC0 <nicoconfaa@gmail.com>
2022-07-30 14:33:35 -0300

    getGifs converted to async function

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,5 +1,5 @@
 const API_KEY = '9b31XwQcbPSFrMhL0tMb3NJtaSCoQX8a'
-export default async function getGifs({ keyword = 'morty'} = {}) {
+export default async function getGifs({ keyword }) {
   const API_URL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`
 
   const res = await fetch(API_URL)

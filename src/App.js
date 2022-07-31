8826f1fedbe6219904074f16974d46da3c238ab5
NicoC0 <nicoconfaa@gmail.com>
2022-07-30 21:01:34 -0300

    use of wouter (routes) for search of gifs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route } from "wouter";
 import "./App.css";
 import ListOfGifs from "./components/ListOfGifs";
 
@@ -6,7 +7,14 @@ function App() {
   return (
     <div className="App">
       <section className="App-content">
-        <ListOfGifs keyword='Argertina' />
+        <Route
+          path="/gif/:keyword"
+          component={ListOfGifs}
+        />
+        <h1>Gifeate</h1>
+        <a href="/gif/argentina">Gifs de Argentina</a>
+        <a href="/gif/red+panda">Gifs de panda rojo</a>
+        <a href="/gif/fast+and+furious">Gifs de rápido y furioso</a>
       </section>
     </div>
   );

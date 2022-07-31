fe32ec7ad3e75d28b4b7743174c2312948100525
NicoC0 <nicoconfaa@gmail.com>
2022-07-30 21:00:56 -0300

    code refactoring for search gifs

--- src/components/ListOfGifs.jsx
+++ src/components/ListOfGifs.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import Gif from "./Gif";
 import getGifs from "../services/getGifs";
 
-const ListOfGifs = ({ keyword }) => {
+const ListOfGifs = ({ params }) => {
+  const { keyword } = params
   const [gifs, setGifs] = useState([]);
 
   useEffect(() => {
